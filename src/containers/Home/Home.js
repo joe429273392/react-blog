5fb7262363fc4a6a7231370115af2bb1bc05aab5
joe <429273392@qq.com>
2018-05-14 19:42:21 +0800

    first commit

---
+++ src/containers/Home/Home.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import style from './style.css';
+
+class Home extends Component {
+    render() {
+        return (
+            <h1>Tom's Blog</h1>
+        )
+    }
+};
+
+export default Home;
+
